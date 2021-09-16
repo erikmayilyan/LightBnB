1af3d42719f2b5e3b6f1a75d42a38dc16369451c
Mrdo Mrdo <mayilyane9@gmail.com>
2021-09-15 18:42:45 -0700

    Commit or modify all changes

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO users (id, name, email, password) VALUES (1, 'John','johnsmith@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.')
+INSERT INTO users (id, name, email, password) VALUES (2, 'Emily','thebestemail@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (id, name, email, password) VALUES (3, 'Harvard','randomemail@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active) VALUES (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Montreal', 'Quebec', 28142, true); 
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active) VALUES (1, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 1705.61, 7, 5, 6, 'Canada', '536 Madison Avenue', 'Toronto', 'Ontario', 58542, true);
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active) VALUES (2, 'Habit Mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 305.61, 7, 5, 6, 'Canada', '333 Stone Street', 'Vancouver', 'British Columbia', 20519, true);
+
+INSERT INTO reservations (guest_id, property_id, start_date, end_date)
+VALUES (1, 1, '2018-09-11', '2018-09-26'),
+(2, 2, '2019-01-04', '2019-02-01'),
+(3, 3, '2021-10-01', '2021-10-14');
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES (2, 1, 7, 3, 'messages');
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES (2, 2, 7, 2, 'messages');
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) VALUES (2, 3, 7, 1, 'messages');
