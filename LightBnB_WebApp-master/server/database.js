b0325b5e1eb86d037368e1e5e9bce135dd7d8c6a
Mrdo Mrdo <mayilyane9@gmail.com>
2021-09-16 19:10:38 -0700

    Commit all changes

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -93,7 +93,25 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
+  /*
   return getAllProperties(null, 2);
+  */
+  return pool.query(`
+  SELECT properties.*, reservations.*, AVG(property_reviews.rating) AS average_rating 
+  FROM properties
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  JOIN reservations ON reservations.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+  .then(res => {
+    console.log(res.rows);
+    return res.rows;
+  })
+  .catch(err => console.log(err.stack));
 }
 exports.getAllReservations = getAllReservations;
 
